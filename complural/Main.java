5333e372f683c4400db26af2107a3647ae964483
Silvia Munoz Garcia <jsmunoz22@gmail.com>
2023-01-21 22:53:06 +0000

    Save 3

--- complural/Main.java
+++ complural/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		moto1.nomDelVehicle = Yamaha;
 		
 		moto1.arrenca();
+		
 	
 	
 	}	
---
+++ complural/Vehicles.java
@@ -0,0 +1,19 @@
+package complural;
+
+public class Vehicles {
+
+	String nomDelVehicle;
+	int velocitatMàxima;
+	int kilometresRecorreguts;
+	
+	void arrenca(){
+		System.out.println("El Vehicle arrenca");
+	}
+	
+	void apaga(){
+		System.out.println("El Vehicle s'apaga");
+	}
+	void mou(){
+		System.out.println("El Vehicle està en moviment");
+	}
+}
